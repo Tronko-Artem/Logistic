255385fe179315b91c7027167113f644f447bb3c
ht-evth <52500193+ht-evth@users.noreply.github.com>
2022-04-27 21:08:43 +0700

    test db

--- Logistic/Logistic/Program.cs
+++ Logistic/Logistic/Program.cs
@@ -20,10 +20,20 @@ namespace Logistic
         static void Main()
         {
             db = new LogisticContext();
+
+            List<Okei> temp = Program.db.OkeiList.ToList();
+            List<Customer> temp1 = Program.db.CustomersList.ToList();
+            List<Fias> temp2 = Program.db.FiasList.ToList();
+            List<GoodsInOrder> temp3 = Program.db.GoodsInOrderList.ToList();
+            List<Okpo> temp4 = Program.db.OkpoList.ToList();
+            List<Order> temp5 = Program.db.OrdersList.ToList();
+            List<Treatie> temp6 = Program.db.TreatiesList.ToList();
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
