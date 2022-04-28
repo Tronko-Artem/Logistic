d3833b8fb7622ec0f7ee897632b852dd211a3a9c
ht-evth <52500193+ht-evth@users.noreply.github.com>
2022-04-28 19:19:15 +0700

    loading list of customers to datagridview

--- Logistic/Logistic/Counteragents.cs
+++ Logistic/Logistic/Counteragents.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Logistic.Models;
+
 namespace Logistic
 {
     public partial class Counteragents : Form
@@ -15,6 +17,26 @@ namespace Logistic
         public Counteragents()
         {
             InitializeComponent();
+
+            dataGridView1.Columns[0].DataPropertyName = "ID_Customer";
+            dataGridView1.Columns[1].DataPropertyName = "Organization_Name";
+            dataGridView1.Columns[2].DataPropertyName = "Organization_Form";
+            dataGridView1.Columns[3].DataPropertyName = "Director_FIO";
+            dataGridView1.Columns[4].DataPropertyName = "Law_Address";
+            dataGridView1.Columns[5].DataPropertyName = "Mail_Address";
+            dataGridView1.Columns[6].DataPropertyName = "INN";
+            dataGridView1.Columns[7].DataPropertyName = "KPP";
+            dataGridView1.Columns[8].DataPropertyName = "Checking_Account";
+            dataGridView1.Columns[9].DataPropertyName = "Korr_Account";
+            dataGridView1.Columns[10].DataPropertyName = "BIK";
+            dataGridView1.Columns[11].DataPropertyName = "OKPO";
+
+        }
+
+        private void updateTable()
+        {
+            List<Customer> customers = Program.db.CustomersList.ToList();
+            dataGridView1.DataSource = customers;
         }
 
         private void AddCounteragentButton_Click(object sender, EventArgs e)
@@ -41,6 +63,17 @@ namespace Logistic
             CorrAccountTextBox.Text = "";
             BikTextBox.Text = "";
             OkpoTextBox.Text = "";
+        }
+
+        private void Counteragents_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void Counteragents_Activated(object sender, EventArgs e)
+        {
+            updateTable();
         }
     }
 }
