ac23fb83e5596b67fa86cf30ced55e5dc64d9b22
ht-evth <52500193+ht-evth@users.noreply.github.com>
2022-04-29 20:54:49 +0700

    implemented load and search

--- Logistic/Logistic/TreatieRegistry.cs
+++ Logistic/Logistic/TreatieRegistry.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Logistic.Models;
+
 namespace Logistic
 {
     public partial class TreatieRegistry : Form
@@ -15,6 +17,23 @@ namespace Logistic
         public TreatieRegistry()
         {
             InitializeComponent();
+
+            CounteragentListBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            CounteragentListBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            dataGridView1.Columns[0].DataPropertyName = "ID_Treatie";
+            dataGridView1.Columns[1].DataPropertyName = "Number_Of_Treatie";
+            dataGridView1.Columns[2].DataPropertyName = "Start_Date";
+            dataGridView1.Columns[3].DataPropertyName = "End_Date";
+            dataGridView1.Columns[4].DataPropertyName = "ID_Customer";
+            dataGridView1.Columns[5].DataPropertyName = "Summary";
+            dataGridView1.Columns[6].DataPropertyName = "Status";
+        }
+
+        private void updateTable()
+        {
+            List<Treatie> treaties = Program.db.TreatiesList.ToList();
+            dataGridView1.DataSource = treaties;
         }
 
         private void ExitButton_Click(object sender, EventArgs e)
@@ -60,6 +79,7 @@ namespace Logistic
         {
             TreatieNumberTextBox.Text = "";
             CounteragentListBox.Text = "";
+            CounteragentListBox.SelectedIndex = -1;
             SummaryTextBox.Text = "";
             StatusListBox.SelectedItem = "Не выбрано";
             StartDateCheckBox.Checked = false;
@@ -70,6 +90,98 @@ namespace Logistic
         {
             Form TreatieExport = new TreatieExport();
             TreatieExport.Show();
+        }
+
+        private void TreatieRegistry_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void TreatieRegistry_Activated(object sender, EventArgs e)
+        {
+            Customer selectedCustomer = ( ( Customer )CounteragentListBox.SelectedItem );
+            List<Customer> customers = Program.db.CustomersList.ToList();
+            CounteragentListBox.DataSource = customers;
+
+            if( selectedCustomer != null && customers.Contains( selectedCustomer ) )
+                CounteragentListBox.SelectedItem = selectedCustomer;
+            else
+                CounteragentListBox.SelectedItem = null;
+
+            FindButton.PerformClick();
+        }
+
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            if( String.IsNullOrWhiteSpace( CounteragentListBox.Text ) )
+            {
+                CounteragentListBox.SelectedItem = null;
+                CounteragentListBox.SelectedIndex = -1;
+            }
+
+            if( String.IsNullOrWhiteSpace( StatusListBox.Text ) || StatusListBox.SelectedItem == null || StatusListBox.SelectedIndex == -1 )
+            {
+                StatusListBox.SelectedIndex = 0;
+            }
+
+            if( String.IsNullOrWhiteSpace( TreatieNumberTextBox.Text ) ) TreatieNumberTextBox.Text = "";
+            if( String.IsNullOrWhiteSpace( SummaryTextBox.Text ) ) SummaryTextBox.Text = "";
+
+            Decimal summary = -1;
+            try
+            {
+                summary = Decimal.Parse( SummaryTextBox.Text );
+
+                if (summary < 0)
+                    throw new Exception();
+            }
+            catch( Exception /*exception */ )
+            {
+                SummaryTextBox.Text = "";
+                summary = -1;
+            }
+
+
+            List<Treatie> allData = Program.db.TreatiesList.ToList();
+            List<Treatie> searchResult = new List<Treatie>();
+
+            int ID_Customer = -1;
+            if (this.CounteragentListBox.SelectedItem != null)
+                ID_Customer = ( (Customer)this.CounteragentListBox.SelectedItem ).ID_Customer;
+
+            bool checkStartDate = this.StartDateCheckBox.Checked;
+            bool checkEndDate = this.EndDateCheckBox.Checked;
+
+                      
+            foreach (Treatie item in allData )
+            {
+                if (StatusListBox.SelectedIndex > 0 && item.Status != StatusListBox.Text)
+                    continue;
+
+                if( checkStartDate && ( item.Start_Date < StartDateListBox1.Value.Date || item.Start_Date > StartDateListBox2.Value.Date ) )
+                    continue;
+
+                if( checkEndDate && ( item.End_Date < EndDateListBox1.Value.Date || item.End_Date > EndDateListBox2.Value.Date ) )
+                    continue;
+
+                if (ID_Customer != -1 && item.ID_Customer != ID_Customer)
+                    continue;
+                
+                if( !item.Number_Of_Treatie.ToLower().Contains( TreatieNumberTextBox.Text ) )
+                    continue;
+
+                if( summary >= 0 && item.Summary != summary )
+                    continue;
+
+
+                searchResult.Add(item);
+            }
+
+
+            dataGridView1.DataSource = searchResult;
+            dataGridView1.ClearSelection();
+            
         }
     }
 }
